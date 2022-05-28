a1637bff98bdd5525a7a78847a285f4828a91b84
TyroneKong <tyronekong@gmail.com>
2022-05-28 03:38:52 +0100

    added env to gitignore

--- FinerVisionform/src/App.jsx
+++ FinerVisionform/src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 import { Form } from "./form/Form";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <Form />
